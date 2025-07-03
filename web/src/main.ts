583a70e650592073cf1e41cdd65a2e2fc33495ab
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2025-07-02 19:03:22 -0500

    Ref: example code transpiled into wasm using wasm-pack crate

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,32 +1,41 @@
+import init, { greet } from 'game-core';
+
 async function main(): Promise<void> {
-  const memory = new WebAssembly.Memory({ initial: 1, maximum: 10 });
-
-  const importObject = {
-    env: {
-      mem: memory,
-    },
-    console: {
-      log: () => {
-        console.log('Hello from TS!');
-      },
-      error: () => {
-        console.error('Error from TS!');
-      },
-    },
-  };
-
-  const { instance } = await WebAssembly.instantiateStreaming(
-    fetch('/example.wasm'),
-    importObject
-  );
-
-  const uint8Array = new Uint8Array(memory.buffer, 0, 23);
-  const text = new TextDecoder('utf-8').decode(uint8Array);
-  console.log(text);
-
-  const sum = instance.exports.sum as CallableFunction;
-  const result = sum(100, 300);
-  console.log(result);
+  void await init();
+  greet('World');
 }
 
 main();
+
+// async function main(): Promise<void> {
+//   const memory = new WebAssembly.Memory({ initial: 1, maximum: 10 });
+//
+//   const importObject = {
+//     env: {
+//       mem: memory,
+//     },
+//     console: {
+//       log: () => {
+//         console.log('Hello from TS!');
+//       },
+//       error: () => {
+//         console.error('Error from TS!');
+//       },
+//     },
+//   };
+//
+//   const { instance } = await WebAssembly.instantiateStreaming(
+//     fetch('/example.wasm'),
+//     importObject
+//   );
+//
+//   const uint8Array = new Uint8Array(memory.buffer, 0, 23);
+//   const text = new TextDecoder('utf-8').decode(uint8Array);
+//   console.log(text);
+//
+//   const sum = instance.exports.sum as CallableFunction;
+//   const result = sum(100, 300);
+//   console.log(result);
+// }
+//
+// main();
