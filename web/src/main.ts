4ba107d94945ee449ad9737766224485b02c2f5e
Nicolás Rodríguez <nicrodriguezval@gmail.com>
2025-12-31 20:09:36 -0500

    Add wee_alloc dependency and implement world drawing in main.ts

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,10 +1,38 @@
 import init, { World } from './wasm/pkg/game_core.js';
 
+const CELL_SIZE = 20;
+
+function drawWorld(ctx: CanvasRenderingContext2D, world: World): void {
+  ctx.beginPath();
+
+  for (let col = 0;  col < world.size + 1; col++) {
+    ctx.moveTo(col * CELL_SIZE, 0);
+    ctx.lineTo(col * CELL_SIZE, world.size * CELL_SIZE);
+  }
+
+  for (let row = 0; row < world.size + 1; row++) {
+    ctx.moveTo(0, row * CELL_SIZE);
+    ctx.lineTo(world.size * CELL_SIZE, row * CELL_SIZE);
+  }
+
+  ctx.stroke();
+}
+
 async function main(): Promise<void> {
   void await init();
+
   const world = World.new(8);
+
   const canvas = document.getElementById('snake-game-canvas') as HTMLCanvasElement;
   const ctx = canvas.getContext('2d');
+  if (ctx === null) {
+    throw new Error('Failed to get 2D context');
+  }
+
+  canvas.height = world.size * CELL_SIZE;
+  canvas.width = world.size * CELL_SIZE;
+
+  drawWorld(ctx, world);
 }
 
 await main();
